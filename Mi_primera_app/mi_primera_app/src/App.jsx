f24794d6aaf9d349ee6a927e1cbdc8f2ef141317
jobarv <jbarrera@motiontech.com.mx>
2025-12-16 20:51:15 -0600

    Hooks y Components

--- Mi_primera_app/mi_primera_app/src/App.jsx
+++ Mi_primera_app/mi_primera_app/src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from "./components/Header"
+import Card from './components/Card'
+import Prueba from './components/Prueba'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Prueba />
+      <Card title="ESto es un titulo" description="Descripcion desde componente" />
+      <Card title="esto es otro titulo" description="Descripcion desde componente" />
+      <Card title="Mas titulos" description="Descripcion desde componente" />
+      <Card title="Uno mas para terminar" description="Descripcion desde componente" />
+
+
+      <h1>Hola Mundo</h1>
+
+
     </>
   )
 }
